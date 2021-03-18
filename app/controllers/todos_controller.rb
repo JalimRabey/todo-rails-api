b7965240988e2e6fa3752671f2acb748ffce840e
Jalim Rabey <123risada@gmail.com>
2021-03-17 21:53:39 -0300

    feat: toggle todo completed

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: [:show, :update, :destroy]
+  before_action :set_todo, only: [:show, :update, :destroy, :toggle_completed]
 
   # GET /todos
   def index
@@ -36,6 +36,16 @@ class TodosController < ApplicationController
   # DELETE /todos/1
   def destroy
     @todo.destroy
+  end
+
+  def toggle_completed
+    @todo[:completed] = !@todo[:completed]
+
+    if @todo.save
+      render json: @todo, status: :ok
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
   end
 
   private
